15ec1135b970ad3fa0380215e45ecd3e13b5fdfd
james <jamessoh17@gmail.com>
2025-03-28 18:30:30 -0400

    new model updated

--- DetectorTemplate/DetectorCode/detector.py
+++ DetectorTemplate/DetectorCode/detector.py
@@ -86,6 +86,7 @@ class Detector(ADetector):
 
 
 ################## 2. Evaluate the data with the model #######################
+# Based on T3/prediction.py
     def _calculate_confidence(self, unlabeled_processed_data):
         MODEL_PATH = 'DetectorTemplate/DetectorCode/best_model.pt'                      
         BATCH_SIZE = 16                                   
